b4551d33ef3e5af497bb2e025fbed0842e1148a0
JiaweiLu1999 <jl5999@columbia.edu>
2023-07-04 17:02:44 -0400

    v=wr

--- Driver.cs
+++ Driver.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float w = 180f / (80 * Mathf.PI);
     // Start is called before the first frame update
     void Start()
     {
         
+        Debug.Log(w);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        transform.Rotate(0, 0, w);
+        transform.Translate(0, 0.1f, 0);
     }
 }
